ff718c8a23d93fb2d81111ab8c806c55889f8158
soumyadip-schl <schl.soumyadipkarforma@gmail.com>
2025-07-12 19:12:04 +0530

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,13 @@
+{
+  "compilerOptions": {
+    "target": "ES2021",
+    "module": "CommonJS",
+    "rootDir": "./",
+    "outDir": "./dist",
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true
+  },
+  "include": ["server", "shared"]
+}
