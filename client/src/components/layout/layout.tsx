e6c5abf3ca5713c3f9afe01822a5b49cb6b957a4
soumyadip-schl <schl.soumyadipkarforma@gmail.com>
2025-07-19 19:24:53 +0530

    Update layout.tsx

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "./header";
 import Footer from "./footer";
 import NewsTicker from "../news-ticker";
-import Navbar from "../navbar"; // Add/adjust path if your Navbar is elsewhere
+import Navbar from "../navbar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,11 +9,11 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen w-full max-w-screen bg-white dark:bg-black text-black dark:text-white overflow-x-hidden m-0 p-0 flex flex-col">
+    <div className="min-h-screen w-screen overflow-x-hidden m-0 p-0 flex flex-col bg-white dark:bg-black text-black dark:text-white">
       <Navbar />
       <Header />
       <NewsTicker />
-      <main className="flex-1 pt-16">
+      <main className="flex-1 pt-16 w-full">
         {children}
       </main>
       <Footer />
